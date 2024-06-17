511eaccbb90592f108f2108138c74abeac11a8b7
thisisbhanuj <tobhanuj@gmail.com>
2024-06-17 20:26:11 +1000

    Composite Component : TaskLists added

---
+++ src/components/tasks/TaskList.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import Task from './Task';
+
+interface TaskListProps {
+    loading: boolean;
+    tasks: {
+        id: string;
+        title: string;
+        state: string;
+    }[];
+    onPinTask: (id: string) => void;
+    onArchiveTask: (id: string) => void;
+}
+
+export default function TaskList({ loading, tasks, onPinTask, onArchiveTask } : Readonly<TaskListProps>) {
+    const events = {
+        onPinTask,
+        onArchiveTask,
+      };
+    const LoadingRow = (
+    <div className="loading-item">
+        <span className="glow-checkbox" />
+        <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+        </span>
+    </div>
+    );
+    if (loading) {
+    return (
+        <div className="list-items" data-testid="loading" key={"loading"}>
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        </div>
+    );
+    }
+    if (tasks.length === 0) {
+    return (
+        <div className="list-items" key={"empty"} data-testid="empty">
+        <div className="wrapper-message">
+            <span className="icon-check" />
+            <p className="title-message">You have no tasks</p>
+            <p className="subtitle-message">Sit back and relax</p>
+        </div>
+        </div>
+    );
+    }
+    
+    const tasksInOrder = [
+    ...tasks.filter((t) => t.state === 'TASK_PINNED'),
+    ...tasks.filter((t) => t.state !== 'TASK_PINNED'),
+    ];
+    return (
+    <div className="list-items">
+        {tasksInOrder.map((task) => (
+        <Task key={task.id} task={task} {...events} />
+        ))}
+    </div>
+    );
+}
